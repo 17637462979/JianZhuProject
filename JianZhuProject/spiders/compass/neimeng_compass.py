0d6e9e9bb9f91b614d4597df642f85abeba988eb
dataspider <2055381145@qq.com>
2018-12-29 19:12:45 +0800

    modify

--- JianZhuProject/spiders/compass/neimeng_compass.py
+++ JianZhuProject/spiders/compass/neimeng_compass.py
@@ -13,27 +13,34 @@ class NeiMongCompass(BaseCompass):
     name = 'neimong_compass'
     allow_domain = ['110.16.70.26']
     custom_settings = {
-        # 'ITEM_PIPELINES': {'JianZhuProject.CorpNamePipeline.CorpNamePipeline': 300,},
+        'ITEM_PIPELINES': {'JianZhuProject.CorpNamePipeline.CorpNamePipeline': 300, },
         # 'ALL_FINGER_CONTAINS': 'finger_contains_new1_tmp'
     }
-    start_urls = [
+    start_urls = [  # 30                       # 1                    # 593
         (
         'http://110.16.70.26/nmjgpublisher/handle/ProjectsInfoHandler.ashx?type=CorpInfo&lblPageCount={total_page}&lblPageIndex={cur_page}&lblRowsCount={total_rows}&lblPageSize=20',
         sit_list[0]),  #
+        # ('http://110.16.70.26/nmjgpublisher/corpinfo/CorpInfoObtain.aspx', sit_list[0])
     ]
 
     extract_dict = {
         'inner': {
             'nodes': '//tr',
-            'cname': './a[contains(@onclick, "OnReturn")]/text()',
-            'detail_link': './a[contains(@onclick, "OnReturn")]/@onclick',
-        # "OnReturn('911508001172071957','巴彦淖尔市第三建筑有限责任公司')"
+            'cname': './td[contains(@class, "company_name") and @title]/@title',
+            # 'cname': './a[contains(@onclick, "OnReturn")]//text()',
+            'detail_link': './/a[contains(@onclick, "OnReturn")]/@onclick',
             'out_province': ['None', 'neimonggu'],
         },
     }
 
-    def parse_list(self, response):
+    def start_requests(self):
+        for link, sit in self.start_urls:
+            full_link = link.format(total_page=30, cur_page=1, total_rows=593)
+            yield scrapy.Request(full_link, callback=self.parse_list,
+                                 meta={'total_page': 30, 'cur_page': 1, 'link': link})
 
+
+    def parse_list(self, response):
         json_resp = json.loads(response.text)
         total_page = json_resp['nPageCount']
         total_rows = json_resp['nPageRowsCount']
@@ -41,35 +48,30 @@ class NeiMongCompass(BaseCompass):
         html_str = json_resp['tb']
 
         item_contains = []
-        html = etree.HTML(html_str, 'lxml')
+        html = etree.HTML(html_str)
         nodes = html.xpath(self.extract_dict['inner']['nodes'])
         for node in nodes:
             item = NameItem()
-            item['cname'] = node.xpath(self.extract_dict['inner']['cname']).extract_first()
+            item['compass_name'] = node.xpath(self.extract_dict['inner']['cname'])[0]
             item['detail_link'] = self.handle_cdetail_link(
-                node.xpath(self.extract_dict['inner']['detail_link']).extract_first())
+                node.xpath(self.extract_dict['inner']['detail_link'])[0])
             item['out_province'] = self.extract_dict['inner']['out_province'][1]
             item_contains.append(item)
-        yield item_contains
+        yield {'item_contains': item_contains}
 
         if int(cur_page_num) < int(total_page):
-            self.turn_page(response)
+            yield self.turn_page(response)
         else:
-            print('不能再翻页了，当前页码:', cur_page_num)
-            print(response.text)
+            print(u'不能再翻页了，当前页码:', cur_page_num)
             return
 
     def turn_page(self, response):
         url = response.url
-        next_page_no = response.xpath(self.extract_dict['inner']['turn_page_no']).extract_first()
-        last_page_no = response.xpath(self.extract_dict['inner']['last_page_no']).extract_first()
-        if next_page_no == last_page_no:
-            print('不能继续翻页了，当前最大页码:', next_page_no)
-            return
         headers = self.get_header(response.url, flag='2')
         meta = response.meta
-
-        return scrapy.Request(url, headers=headers, callback=self.parse_list, meta=meta)
+        meta['cur_page'] = str(int(meta['cur_page']) + 1)
+        link = meta['link'].format(total_page=30, cur_page=meta['cur_page'], total_rows=593)
+        return scrapy.Request(link, headers=headers, callback=self.parse_list, meta=meta)
 
     def get_header(self, url, flag='1'):
         headers = {
@@ -77,15 +79,13 @@ class NeiMongCompass(BaseCompass):
             'Host': '110.16.70.26',
             'Referer': url
         }
-        # if flag not in (1, '1'):
-        #     headers['Referer'] = url
         return headers
 
     def handle_cdetail_link(self, clink, flag='outer', url=''):
         # OnReturn('91152202743857259K','阿尔山市白狼天原林产有限责任公司')
         import re
-        pp = re.compile(r"OnReturn\('(.*?)','(.*?)'\);")
-        [CorpCode, cname] = re.findall(pp, clink)[0]
+        pp = re.compile(r"OnReturn\('(.*?)','(.*)'\)")
+        [CorpCode, cname] = re.search(pp, clink).groups()
         link = 'http://110.16.70.26/nmjgpublisher/corpinfo/CorpDetailInfoObtain.aspx?CorpCode={}&CorpName={}&VType=1'.format(
             CorpCode, cname)
         return link
