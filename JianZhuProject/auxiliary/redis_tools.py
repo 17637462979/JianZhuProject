88b19bcb9148a9500cb0f6f49ae44994e6293e93
Administrator <study>
2019-01-01 18:06:00 +0800

    Default Changelist

--- JianZhuProject/auxiliary/redis_tools.py
+++ JianZhuProject/auxiliary/redis_tools.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from __future__ import print_function
 import redis
 
 from JianZhuProject.settings import ALL_FINGER_CONTAINS
@@ -9,7 +10,7 @@ class RedisTools():
         self.host = '127.0.0.1'
         self.port = 6379
         self.red_conn = redis.Redis(self.host, self.port, db=5)
-        print '成功连接redis'
+        print(u'成功连接redis')
 
     def check_finger(self, finger, name=ALL_FINGER_CONTAINS):
         is_member = self.red_conn.sismember(name, value=finger)  # 公司容器
@@ -18,6 +19,4 @@ class RedisTools():
     def store_finger(self, name, finger):
         self.red_conn.sadd(name, finger)
         # print(name, finger)
-        print u'指纹%s存储成功' % finger
-
-
+        print(u'指纹%s存储成功' % finger)
