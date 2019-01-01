218d81bdd7d2a419cf436d1a10d46b582951abc9
dataspider <2055381145@qq.com>
2019-01-02 01:02:03 +0800

    Merge branch 'master' of https://github.com/UsingStuding/JianZhuProject
    # Conflicts:
    #	JianZhuProject/spiders/compass/chongqing_compass.py

--- JianZhuProject/spiders/compass/guangdongpart01_compass.py
+++ JianZhuProject/spiders/compass/guangdongpart01_compass.py
@@ -11,16 +11,16 @@ from JianZhuProject.spiders.compass.base_compass import BaseCompass
 
 
 class GuangDongPart01Compass(BaseCompass):
-    name = 'guangdong01_compass'
+    name = 'guangdong_compass'
     allow_domain = ['219.129.189.10:8080']
     custom_settings = {
-        # 'ITEM_PIPELINES': {'JianZhuProject.CorpNamePipeline.CorpNamePipeline': 300, }
+        'ITEM_PIPELINES': {'JianZhuProject.CorpNamePipeline.CorpNamePipeline': 300, }
     }
     log_file = '../logs/{}_log.log'.format(name)
     cnt = 1
     start_urls = [
-        ("http://219.129.189.10:8080/yjcxk/web-nav/enterprises?pageNumber=1", sit_list[0]),
-        ("http://219.129.189.10:8080/yjcxk/web-nav/enterprises?pageNumber=0", sit_list[1]),
+        # ("http://219.129.189.10:8080/yjcxk/web-nav/enterprises?pageNumber=1", sit_list[0]),
+        # ("http://219.129.189.10:8080/yjcxk/web-nav/enterprises?pageNumber=0", sit_list[1]),
         ("http://219.129.189.10:8080/yjcxk/web-nav/persons?pageNumber=1&pageSize=17550", sit_list[0])
     ]
 
@@ -28,11 +28,12 @@ class GuangDongPart01Compass(BaseCompass):
 
     def start_requests(self):
         for link, sit in self.start_urls:
-            yield scrapy.Request(link, callback=self.parse_list)
+            yield scrapy.Request(link, callback=self.parse_list, dont_filter=True)
 
     def parse_list(self, response):
         data = json.loads(response.body_as_unicode())['data']['rows']
         item_contains = []
+        print(u'有', len(data))
         for unit in data:
             if 'persons' in response.url:
                 compass_name = unit['entName']
@@ -42,7 +43,7 @@ class GuangDongPart01Compass(BaseCompass):
                 compass_name = unit['companyName']
                 detail_link = 'http://219.129.189.10:8080/yjcxk/vueStatic/html/companyDetail.jsp?id=' + unit['id']
                 out_province = 'guangdong'
-            if detail_link == '':
+            if detail_link in ('', 'None'):
                 if self.redis_tools.check_finger(compass_name):
                     continue
             else:
