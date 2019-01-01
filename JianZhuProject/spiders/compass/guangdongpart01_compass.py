20f539e1a6e09f2a166590e5fcb4458306a0ab87
Administrator <study>
2019-01-02 02:23:26 +0800

    Default Changelist

--- JianZhuProject/spiders/compass/guangdongpart01_compass.py
+++ JianZhuProject/spiders/compass/guangdongpart01_compass.py
@@ -14,7 +14,7 @@ class GuangDongPart01Compass(BaseCompass):
     name = 'guangdong01_compass'
     allow_domain = ['219.129.189.10:8080', 'www.jyjzcx.com', 'www.zsjs.gov.cn']
     custom_settings = {
-        'ITEM_PIPELINES': {'JianZhuProject.CorpNamePipeline.CorpNamePipeline': 300, }
+        # 'ITEM_PIPELINES': {'JianZhuProject.CorpNamePipeline.CorpNamePipeline': 300, }
     }
     log_file = '../logs/{}_log.log'.format(name)
     cnt = 1
@@ -23,14 +23,15 @@ class GuangDongPart01Compass(BaseCompass):
         # ("http://219.129.189.10:8080/yjcxk/web-nav/enterprises?pageNumber=0", sit_list[1]),
         # ("http://219.129.189.10:8080/yjcxk/web-nav/persons?pageNumber=1&pageSize=17550", sit_list[0])
         # ('http://www.jyjzcx.com/web/companylist.action?pageNum=1&pageSize=15', sit_list[0])
-        ('http://www.zsjs.gov.cn/web/enterprise/findEnterprises?page=1&start=45', sit_list[0])
-
+        # ('http://www.zsjs.gov.cn/web/enterprise/findEnterprises?page=1&start=45', sit_list[0]),
+        ('https://gcjs.sg.gov.cn/website/buildproject/buildProjectSjAction!proMainList.action?pager.offset=20',
+         sit_list[0]),
     ]
     extract_dict = {
         'inner': {
-            'nodes': '//div[@class="list"]/table//tr[position()>1]',
-            'cname': './td[@class="tdl"]/a/text()',
-            'detail_link': './td[@class="tdl"]/a/@href',  # 'http://www.jyjzcx.com' + xxx,
+            'nodes': '//table[@class="list_div"]//tr',
+            'cname': './td/a/text()',
+            'detail_link': './td/a/@href',  # https://gcjs.sg.gov.cn     #'http://www.jyjzcx.com' + xxx,
             'next_page_url': '//a[@class="laypage_next"]/@data-page'  #
         }
     }
@@ -39,7 +40,7 @@ class GuangDongPart01Compass(BaseCompass):
 
     def start_requests(self):
         for link, sit in self.start_urls:
-            yield scrapy.Request(link, callback=self.parse_list2, meta={'cur_page': '1'})
+            yield scrapy.Request(link, callback=self.parse_list1, meta={'cur_page': '1'})
 
     def parse_list1(self, response):
         ext_rules = self.extract_dict['inner']
@@ -47,8 +48,8 @@ class GuangDongPart01Compass(BaseCompass):
         item_contains = []
         for node in nodes:
             item = NameItem()
-            item['compass_name'] = node.xpath(ext_rules['cname']).extract_first()
-            item['detail_link'] = node.xpath(ext_rules['detail_link']).extract_first()
+            item['compass_name'] = self.handle_cname(node.xpath(ext_rules['cname']).extract_first())
+            item['detail_link'] = self.handle_cdetail_link(node.xpath(ext_rules['detail_link']).extract_first())
             item['out_province'] = 'waisheng'
             if self.redis_tools.check_finger(item['detail_link']):
                 print(u'{}已经爬取过'.format(item['compass_name']))
@@ -59,13 +60,15 @@ class GuangDongPart01Compass(BaseCompass):
 
     def turn_page(self, response):
         meta = response.meta
-        if int(meta['cur_page']) >= 19:
+        if int(meta['cur_page']) >= 4:
             print(u'不能在翻页了')
             return
         meta['cur_page'] = str(int(meta['cur_page']) + 1)
-        link = 'http://www.jyjzcx.com/web/companylist.action?pageNum={}&pageSize=15'.format(meta['cur_page'])
+        # link = 'http://www.jyjzcx.com/web/companylist.action?pageNum={}&pageSize=15'.format(meta['cur_page'])
         headers = self.get_header(response.url, flag='2')
-        return scrapy.Request(link, callback=self.parse_list1, headers=headers, meta=meta)
+        form_data = self.get_form_data(response)
+        return scrapy.FormRequest(response.url, formdata=form_data, callback=self.parse_list1, headers=headers,
+                                  meta=meta)
 
     def parse_list2(self, response):
         json_data = json.loads(response.body_as_unicode())
@@ -127,6 +130,35 @@ class GuangDongPart01Compass(BaseCompass):
         #     #     next_page_num = response['']
         #     #     "http://219.129.189.10:8080/yjcxk/web-nav/persons?pageNumber={}&pageSize=5000".format(next_page_num)
         #     #     return
+
+    def handle_cname(self, cname):
+        return cname.replace('企业基本信息', '')
+
+    def handle_cdetail_link(self, link):
+
+        return 'https://gcjs.sg.gov.cn' + link
+
+    def get_form_data(self, resp):
+        meta = resp.meta
+        formdata = {
+            'tId': '',
+            'tpId': '3',
+            'areaCode': '440200',
+            'gkmlbh2': '',
+            'gkywbt': '',
+            'startTime': '',
+            'endTime': '',
+            'pagesize': '20',
+            'pageNumber': str(meta['cur_page']),
+            'SYSTEM_YANGANG_IS_RESET_SEARCH': 'false',
+            'currentpage': str(meta['cur_page']),
+        }
+        if 'buildProjectS' in resp.url:
+            formdata['gkmlbh'] = 'XYDW_QYZZ'
+        else:
+            formdata['gkmlbh'] = 'XYDW_QYJB'
+
+        return formdata
 
 
 if __name__ == '__main__':
