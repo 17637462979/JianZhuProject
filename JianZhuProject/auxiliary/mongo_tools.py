1f69c28f22758c1fd9971c8617ce3b772f4361bb
dataspider <2055381145@qq.com>
2019-01-02 00:19:02 +0800

    Merge branch 'master' of https://github.com/UsingStuding/JianZhuProject
    # Conflicts:
    #	JianZhuProject/spiders/compass/chongqing_compass.py

--- JianZhuProject/auxiliary/mongo_tools.py
+++ JianZhuProject/auxiliary/mongo_tools.py
@@ -1,5 +1,6 @@
 
 # coding=utf-8
+from __future__ import print_function
 import pymongo
 
 class MongoTools():
@@ -8,7 +9,7 @@ class MongoTools():
         self.port = 27017
         self.mongo_conn = pymongo.MongoClient(self.host, self.port)
 
-        print '成功连接mongo'
+        print('成功连接mongo')
 
     def get_documents(self, batch_size, skip_num):
         result = self.mongo_conn.zjsc.compass_table.find().limit(int(batch_size)).skip(int(skip_num))
